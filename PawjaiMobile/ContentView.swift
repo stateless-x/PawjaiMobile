faea500a524ca568373ffbeea35b77fdd77f846d
stateless-x <purin.buriwong@gmail.com>
2025-09-03 18:07:38 +0700

    Initial Commit

---
+++ PawjaiMobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PawjaiMobile
+//
+//  Created by Purin Buriwong on 3/9/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PawjaiMobile/PawjaiMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawjaiMobileApp.swift
+//  PawjaiMobile
+//
+//  Created by Purin Buriwong on 3/9/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct PawjaiMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
